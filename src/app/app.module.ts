17ba3e880cb6dbbb05225496e732cc94fa02c9e5
Mihail Schutow <schutow.mihail@yandex.ru>
2022-11-05 11:55:18 +0300

    update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {Test1Component} from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test7Component } from './test7/test7.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
-    Test2Component
+    Test2Component,
+    Test3Component,
+    Test7Component
   ],
   imports: [
     BrowserModule
